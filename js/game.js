19f2ca1d244ae3cb04f26763418b53c9e730695f
Ioana <99274396+Ioana-Mar@users.noreply.github.com>
2022-06-03 14:56:25 +0000

    game.js file completed (for now)

--- js/game.js
+++ js/game.js
@@ -2,5 +2,12 @@
 
 // hold perameters for basic phaser game setup
 const config = {
-  
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1008,
+  backgroundcolor: 0x5f6e7a
 }
+
+// creates a new game with setup info
+const game = new Phaser.Game(config)
+console.log(game)
