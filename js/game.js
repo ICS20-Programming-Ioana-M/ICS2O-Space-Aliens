9cce8f8f1f963709c9f4ac7b5f723373cb64328b
Ioana <99274396+Ioana-Mar@users.noreply.github.com>
2022-06-04 02:37:50 +0000

    1-5 done

--- js/game.js
+++ js/game.js
@@ -1,13 +1,43 @@
-/* Global Phaser */
+/* global Phaser */
 
-// hold perameters for basic phaser game setup
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This is the Phaser3 configuration file
+
+import SplashScene from './splashScene.js'
+import TitleScene from './titleScene.js'
+
+// Our game scenes
+const splashScene = new SplashScene()
+const titleScene = new TitleScene()
+
+// Game scene startup info
 const config = {
   type: Phaser.AUTO,
   width: 1920,
-  height: 1008,
-  backgroundcolor: 0x5f6e7a
+  height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false
+    }
+  },
+  // set background color
+  backgroundColor: 0xffffff,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // placed in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
-// creates a new game with setup info
 const game = new Phaser.Game(config)
-console.log(game)
+
+// loads scenes
+game.scene.add('splashScene', splashScene)
+game.scene.add('titleScene', titleScene)
+
+// start title
+game.scene.start('splashScene')
---
+++ js/splashScene.js
@@ -0,0 +1,38 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Ioana All rights reserved
+//
+// Created by: Ioana Marinescu
+// Created on: June 2022
+// For the splash scene
+
+class SplashScene extends Phaser.Scene {
+  constructor () {
+    super({ key: 'splashScene' })
+
+    this.splashSceneBackgroundImage = null
+  }
+
+  init (data) {
+    this.cameras.main.setBackgroundColor('#ffffff')
+  }
+
+  preload () {
+    console.log('Splash Scene')
+    this.load.image('splashSceneBackground', './assets/imh.jpg')
+  }
+
+  create (data) {
+    this.splashSceneBackgroundImage = this.add.sprite(0, 0, 'splashSceneBackground')
+    this.splashSceneBackgroundImage.x = 1920 / 2
+    this.splashSceneBackgroundImage.y = 1080 / 2
+  }
+
+  update (time, delta) {
+    if (time > 3000) {
+      this.scene.switch('titleScene')
+    }
+  }
+}
+
+export default SplashScene
